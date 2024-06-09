8bdd4090a86e8317dff53e8e02fcd181feff8b46
StringAna <tewaryantara21@gmail.com>
2024-06-08 20:56:27 -0400

    Tagline added

--- src/components/ui/Lamp.js
+++ src/components/ui/Lamp.js
@@ -16,7 +16,9 @@ export function LampDemo() {
         }}
         className='mt-8 bg-gradient-to-br from-slate-300 to-slate-500 py-4 bg-clip-text text-center text-4xl font-medium tracking-tight text-transparent md:text-7xl'
       >
-        Build lamps <br /> no way
+        Tired of coding in the dark?
+        <br />
+        Let's bring you to the light.
       </motion.h1>
     </LampContainer>
   );
